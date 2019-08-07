e95c2e82bdebdd68ea09b9dbc1274fb184bfaef7
Gabreil Nz <y2dcapital@gmail.com>
2019-08-07 14:49:13 +0430

    Lottifies Updated :)

--- app/components/Lottie/Success/index.js
+++ app/components/Lottie/Success/index.js
@@ -1,19 +1,21 @@
-import { AnimatedContainer } from "./style";
-import React, { PureComponent } from "react";
+import { Container } from "./style";
+import React, { Component } from "react";
 import LottieView from "lottie-react-native";
 import { Animated, Dimensions } from "react-native";
 
 const HEIGTH = Dimensions.get("window").height;
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
 
-class Success extends PureComponent {
+class Success extends Component {
   state = {
     top: new Animated.Value(0),
     opacity: new Animated.Value(0)
   };
 
-  componentDidUpdate() {
+  componentDidMount() {
     const { top, opacity } = this.state;
     const { isActive } = this.props;
+
     if (isActive) {
       Animated.timing(top, { toValue: 0, duration: 0 }).start();
       Animated.timing(opacity, { toValue: 1 }).start();
